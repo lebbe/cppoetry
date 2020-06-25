58ed28101e28fef4e700ed572c74b380f6941857
Lebchen <lebchen@gmail.com>
2020-06-25 21:52:39 +0200

    Init.

---
+++ Readme.md
@@ -0,0 +1,25 @@
+
+C̪̞̠͈̣̜̾̎̋̚͝Y̨̙͉̯͉̓̂̐͆̒ͅP̭̪̈̂̒̉̕͘͜E̢̹̙͉̼̖͌͌͑̽Ŗ̙͈̪̰̍̿͋͒̐P̹̭͓͖͎͆̈́U̯͔̓̀̔̓N̐̍͌̃̾͘͜K̫̪͉̟͚̑̉͜ ̛͍͚͋͂͝P̩̾̎͊̽̎͊ͅŐ̩̦Ȩ̬̦͍̮͒͒̏̀͋ͅŢ̖̼̺́̽̒͆R͍͍͗̎Ẏ̧̼͉͜
+===================
+
+
+Spits out a cyberpunk poem. Don't mind the glitches.
+
+```
+ꜱ̑ᴛ͑ᴀ̉ᴛ̔ɪ̐ᴄ̏ ɴ͒ᴇ͝ᴏ̈́ɴ̊ ꜱ̒ʜ͝ᴇ́ᴇ̇ᴘ͛
+ ꜱ̊ᴛ̊ʀ͛ᴀ̚ɴ̅ɢ͘ᴇ̊ ᴄ͋ʀ̾ᴇ̚ᴀ̽ᴛ͂ᴜ͝ʀ̇ᴇ͛
+  ᴇ͜ᴠ̥ɪ̦ʟ͉ ᴍ̭ᴇ̢ᴛ̯ʀ̮ᴏ̣ᴘ̗ᴏ̲ʟ̗ɪ͔ꜱ̻
+      ᴄͅᴀ͖ɴ̰ ᴡ̧ᴇ̻ ꜰ̣ᴀ͍ʟ̡ʟ̪
+```
+
+
+
+```
+       ša͛c͆r͝ìl̄o̎g̊i̊o͝ủs̑ ňèȍn͛ s̃h̎èêp̽
+                   f̈́āl̏ṡȇ s̀h̚e̕ép̊
+                    e̮v̮i͈l̡ r̙o̯ḅo̯t͜s̜
+w̭e͓ a̭r̙e̼ a͜l̟l̮ g̺o̼i̗n͍g̭ t̤o̟ s͙h͔o̦w̫ m̹e͉r͖c̖y͎
+```
+
+
+The poem contains of four lines, inside a JS array.
---
+++ index.js
@@ -0,0 +1,143 @@
+
+const luni = require('lunicode')
+
+var As = [
+  'scarlet',
+  'electrical',
+  'evil',
+  'good',
+  'alien',
+  'strange',
+  'sacrilogious',
+  'drifting',
+  'false',
+  'static',
+  'neon'
+]
+
+var Ns = [
+  'clouds',
+  'sheep',
+  'aliens',
+  'alien',
+  'creature',
+  'robot',
+  'synth',
+  'replicator',
+  'robocop',
+  'robots',
+  'cocksuckers',
+  'metropolis'
+]
+
+var VPs = [
+  'we are all going to',
+  'are we all going to',
+  'everyone must',
+  'please let us',
+  'can we',
+  'must we',
+  'can I',
+  'must I'
+]
+
+var Vs = [
+  'die',
+  'sleep',
+  'cry',
+  'show mercy',
+  'fall',
+  'succumb to misery',
+  'punctuate',
+  'get shot'
+]
+
+
+function random(array) {
+	return array[Math.floor(Math.random() * array.length)]
+}
+
+function mirror(text) {
+	return luni.tools.flip.encode(text)
+}
+
+function tiny(text) {
+	return luni.tools.tiny.encode(text)
+}
+
+function squares(text) {
+	return luni.tools.roundsquares.encode(text)
+}
+
+function bubbles(text) {
+	return luni.tools.bubbles.encode(text.split('').join(' '))
+}
+
+luni.tools.creepify.options.maxHeight = 1
+luni.tools.creepify.options.randomization = 20
+luni.tools.creepify.options.middle = false
+
+function creepyTop(text) {
+	luni.tools.creepify.options.bottom = false
+	luni.tools.creepify.options.top = true
+	return text.split('').map(function(a) {
+		if (a === ' ') return a;
+		return luni.tools.creepify.encode(a)
+	}).join('')
+}
+
+function creepyBottom(text) {
+	luni.tools.creepify.options.bottom = true
+	luni.tools.creepify.options.top = false
+	return text.split('').map(function(a) {
+		if (a === ' ') return a;
+		return luni.tools.creepify.encode(a)
+	}).join('')
+}
+
+
+function generate() {
+	let line1 = random(As) + ' ' + random(As) + ' ' + random(Ns)
+	let line2 = random(As) + ' ' + random(Ns)
+	let line3 = random(As) + ' ' + random(Ns)
+	let line4 = random(VPs) + ' ' + random(Vs)
+
+	// MIRRROOORRR
+	if(Math.random() > 0.95) {
+		line1 = mirror(line1)
+		line2 = mirror(line2)
+		line3 = mirror(line3)
+		line4 = mirror(line4)
+	} else if(Math.random() > 0.97) {
+		line1 = bubbles(line1)
+		line2 = bubbles(line2)
+		line3 = bubbles(line3)
+		line4 = bubbles(line4)
+	} else if(Math.random() > 0.50) {
+		line1 = tiny(line1)
+		line2 = tiny(line2)
+		line3 = tiny(line3)
+		line4 = tiny(line4)
+	}
+
+
+   	// LEFTPADDDDD RIGHT ALIGN!
+	const longest = Math.max(line1.length, Math.max(line2.length, Math.max(line3.length, line4.length)))
+
+	line1 = line1.padStart(longest, ' ')
+	line2 = line2.padStart(longest, ' ')
+	line3 = line3.padStart(longest)
+	line4 = line4.padStart(longest)
+
+
+	// f̷͙̠̿̊ü̴̘̲̒ţ̷̬̯̓u̶͈̎ŕ̷̨͇̱͒è̶̦͍̔̊ ̵̰͑d̴̰͗̑̚ř̷͇̞͝ĕ̵̲͝a̸̖͔̅̀͠m̷̯̦̥̓͂͗s̴̨̏̉ ̶̨̘̿õ̶̱̰̀͝f̴̥͆ ̸͉̀t̶̙͋h̵͓͕͛ę̷̆̉̍ ̶̙̒̿p̸͉̟̽r̶̛̺̔ȇ̴̥̹̳̚͝s̶̭̜͛́́ë̷͙͙́n̸̜̉͠t̶̡̲̖̐̊
+	line1 = creepyTop(line1)
+	line2 = creepyTop(line2)
+	line3 = creepyBottom(line3)
+	line4 = creepyBottom(line4)
+
+	return [line1, line2, line3, line4]
+}
+
+
+module.exports = generate;
