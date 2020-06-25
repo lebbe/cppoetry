00cfa6896c19d14d6e9e2112cb196c8e1e4df9cd
Lebchen <lebchen@gmail.com>
2020-06-25 22:50:23 +0200

    Example irc bot for poetry.

---
+++ bot.js
@@ -0,0 +1,30 @@
+const irc = require('irc')
+const poem = require('./index.js')
+
+console.log(poem())
+
+const client = new irc.Client('irc.network.here', 'replicant', {
+	channels: ['#channel_here']
+})
+.addListener('message', async function (from, to, message) {
+	if(/^\?poem/.test(message)) {
+
+    const p = poem();
+
+    async function fuckWait() {
+      return new Promise(function(resolve) {
+        setTimeout(resolve, 1000)
+      })
+    }
+
+    client.say(to, p[0])
+    await fuckWait()
+    client.say(to, p[1])
+    await fuckWait()
+    client.say(to, p[2])
+    await fuckWait()
+    client.say(to, p[3])
+
+		return
+	}
+})
